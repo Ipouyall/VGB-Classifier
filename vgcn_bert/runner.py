29aaeb4f982ff832188f5a574ac4431cd11948ea
pooyasadeghi <pouyasadeghi2012@gmail.com>
2023-09-02 11:06:15 +0330

    add runner

--- vgcn_bert/runner.py
+++ vgcn_bert/runner.py
@@ -81,3 +81,17 @@ Config(
     {self.device=},
     
 """)
+
+
+class Runner:
+    def __init__(self, config: Config):
+        self.config = config
+
+    def train(self):
+        if self.config.model == "VGCN_BERT":
+            from vgcn_bert.train_vgcn_bert import train as vb_train
+            vb_train(config=self.config)
+        else:
+            print(f"{self.config.model} is not implemented!")
+            Exception()
+
